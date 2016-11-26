82feb2e2417e73896ea7dc59c164d4d1e60bebcf
Fernando <sobralense@gmail.com>
2016-11-26 20:31:40 +0000

    Acrescido interanção reduce e breve explicação do funcionamento e interação

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 const mock = require('../mock.js');
-
 const sections = mock.split(',');
-
 const values = sections.map( section => section.split(';'));
+const rxPage = /[?|&]page=(\d+)/;
+const rxRel = /rel="(.+)"/;
 
-values.map( section => {
-    console.log(section[0]);
-    console.log(section[1]);
+let data1 = values.map( section => {
+    const page = Number(rxPage.exec(section[0])[1]);
+    const rel = rxRel.exec(section[1])[1];
+    return [page, rel];
     });
+
+console.log("Data1: ");
+console.log(data1);
+
+// No Reduce é parecido com MAP, mas necessita de 2 parâmetros.
+// 1o Parâmetro: Callback / 2o Parâmetro: Valor Inicial do reduce.
+// @acc = acumulador
+
+const data2 = data1.reduce((acc, value, index) => {
+    console.log({acc, index}); //Exibe a interação do reduce com objeto.
+    acc[value[1]] = value[0];
+    return acc;
+}, {}); // {}  é o objeto vazio que vai ser preenchido.
+
+console.log("Data2: ");
+console.log(data2);
+
+
