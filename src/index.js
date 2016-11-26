e55ddbdf5939408122f511feaf083e931f77207c
Fernando <sobralense@gmail.com>
2016-11-26 20:54:47 +0000

    Projeto refatorado.

--- src/index.js
+++ src/index.js
@@ -4,26 +4,41 @@ const values = sections.map( section => section.split(';'));
 const rxPage = /[?|&]page=(\d+)/;
 const rxRel = /rel="(.+)"/;
 
-let data1 = values.map( section => {
-    const page = Number(rxPage.exec(section[0])[1]);
-    const rel = rxRel.exec(section[1])[1];
-    return [page, rel];
-    });
+const data = mock.split(',') // Transforma em array de string separado por ';'
+    .map(section => section.split(';')) //Quebra em mais arrays
+    .map(section => { 
+        const page = Number(rxPage.exec(section[0])[1]);
+        const rel = rxRel.exec(section[1])[1];
+        return [page, rel];
+    })
+    .reduce((acc, value, index) => {
+       acc[value[1]] = value[0];
+       return acc
+    }, {});
+    
+console.log(data);
 
-console.log("Data1: ");
-console.log(data1);
+// ** Abaixo Código antes de ser refatorado, para referência futura **
+// let data1 = values.map( section => {
+//     const page = Number(rxPage.exec(section[0])[1]);
+//     const rel = rxRel.exec(section[1])[1];
+//     return [page, rel];
+//     });
 
-// No Reduce é parecido com MAP, mas necessita de 2 parâmetros.
-// 1o Parâmetro: Callback / 2o Parâmetro: Valor Inicial do reduce.
-// @acc = acumulador
+// console.log("Data1: ");
+// console.log(data1);
 
-const data2 = data1.reduce((acc, value, index) => {
-    console.log({acc, index}); //Exibe a interação do reduce com objeto.
-    acc[value[1]] = value[0];
-    return acc;
-}, {}); // {}  é o objeto vazio que vai ser preenchido.
+// // No Reduce é parecido com MAP, mas necessita de 2 parâmetros.
+// // 1o Parâmetro: Callback / 2o Parâmetro: Valor Inicial do reduce.
+// // @acc = acumulador
 
-console.log("Data2: ");
-console.log(data2);
+// const data2 = data1.reduce((acc, value, index) => {
+//     console.log({acc, index}); //Exibe a interação do reduce com objeto.
+//     acc[value[1]] = value[0];
+//     return acc;
+// }, {}); // {}  é o objeto vazio que vai ser preenchido.
+
+// console.log("Data2: ");
+// console.log(data2);
 
 
