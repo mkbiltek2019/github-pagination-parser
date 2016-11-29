e74f24fb8a9ae48b913ce2db5e8914d6f75c9862
sobralense <sobralense@gmail.com>
2016-11-29 02:18:42 -0300

    Tratando erros

--- src/parser.js
+++ src/parser.js
@@ -1,16 +1,20 @@
 const rxPage = /[?|&]page=(\d+)/;
 const rxRel = /rel="(.+)"/;
 
-const parser = str => str.split(',') // Transforma em array de string separado por ';'
-    .map(section => section.split(';')) //Quebra em mais arrays
-    .map(section => {
-        const page = Number(rxPage.exec(section[0])[1]);
-        const rel = rxRel.exec(section[1])[1];
-        return [page, rel];
-    })
-    .reduce((acc, value, index) => {
-        acc[value[1]] = value[0];
-        return acc;
-    }, {});
+const parser = str => {
+  if (typeof str !== 'string'){
+    throw new Error('srt must be a string');
+  }
+  return str.split(','). // Transforma em array de string separado por ';'
+  map(section => section.split(';')). //Quebra em mais arrays
+  map(section => {
+    const page = Number(rxPage.exec(section[0])[1]);
+    const rel = rxRel.exec(section[1])[1];
+    return [page, rel];
+  }).reduce((acc, value, index) => {
+    acc[value[1]] = value[0];
+    return acc;
+  }, {});
+}
 
 module.exports = parser;
--- src/parser.test.js
+++ src/parser.test.js
@@ -2,5 +2,11 @@ const parser = require('./parser');
 const mock = require('../mock');
 
 test('mockfile parsed from parser function', () => {
-  expect(parser(mock)).toEqual({ next: 4, last: 4, first: 1, prev: 2 })
+  expect(parser(mock)).toEqual({next: 4, last: 4, first: 1, prev: 2})
+});
+
+test('handler error', () => {
+  expect(() => {
+    parser()
+  }).toThrowError('srt must be a string');
 });
